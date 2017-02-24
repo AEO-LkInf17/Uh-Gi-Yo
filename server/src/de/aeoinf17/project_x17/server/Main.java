357337fd712d684c73ebc8ba6b9054ca6d9f582d
julian-20 <julian20.delta@gmail.com>
2017-02-24 08:29:57 +0100

    attempt to fix server group

---
+++ server/src/de/aeoinf17/project_x17/server/Main.java
@@ -0,0 +1,10 @@
+package de.aeoinf17.project_x17.server;
+
+/**
+ * Created by jk0521 on 24.02.2017.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("test2");
+    }
+}
