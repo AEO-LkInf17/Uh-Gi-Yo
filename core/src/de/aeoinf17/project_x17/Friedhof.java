29f43b765873ce4d671e47893ac2f1870b559857
Sebastian <sebasti.szymanek@akf-online.com>
2017-02-24 08:26:44 +0100

    klassen erstellt

---
+++ core/src/de/aeoinf17/project_x17/Friedhof.java
@@ -0,0 +1,12 @@
+package de.aeoinf17.project_x17;
+
+import java.util.ArrayList;
+
+/**
+ * Created by schueler on 21.02.2017.
+ */
+public class Friedhof {
+    // Referenzen
+    private Spieler spieler;
+    private ArrayList<Karte> karten;
+}
---
+++ core/src/de/aeoinf17/project_x17/Hand.java
@@ -0,0 +1,12 @@
+package de.aeoinf17.project_x17;
+
+import java.util.ArrayList;
+
+/**
+ * Created by schueler on 21.02.2017.
+ */
+public class Hand {
+    // Referenz
+    private Spieler spieler;
+    private ArrayList<Karte> karten;
+}
