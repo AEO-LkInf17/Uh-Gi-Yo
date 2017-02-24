c9d019b25c7660bbd5415ca17a6e43cab7ff8edd
KosmosSzy <cB0502Asx>
2017-02-24 09:31:03 +0100

    first Methits

--- core/src/de/aeoinf17/project_x17/Monsterkarte.java
+++ core/src/de/aeoinf17/project_x17/Monsterkarte.java
@@ -1,10 +1,32 @@
 package de.aeoinf17.project_x17;
 
-/**
- * Created by schueler on 24.02.2017.
- */
 public class Monsterkarte extends Karte {
     protected int angriffspunkte;
     protected int verteidigungspunkte;
     protected int stufe;
+
+    public void setAngriffspunkte(int angriffspunkte) {
+        this.angriffspunkte = angriffspunkte;
+    }
+
+    public void setVerteidigungspunkte(int verteidigungspunkte) {
+        this.verteidigungspunkte = verteidigungspunkte;
+    }
+
+    public void setStufe(int stufe) {
+        this.stufe = stufe;
+    }
+
+    public int getAngriffspunkte() {
+        return angriffspunkte;
+    }
+
+    public int getVerteidigungspunkte() {
+        return verteidigungspunkte;
+    }
+
+    public int getStufe() {
+        return stufe;
+    }
+
 }
--- core/src/de/aeoinf17/project_x17/Spieler.java
+++ core/src/de/aeoinf17/project_x17/Spieler.java
@@ -18,6 +18,79 @@ public class Spieler {
     private Friedhof friedhof;
 
     // Methoden
+    // getter / setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRang(String rang) {
+        this.rang = rang;
+    }
+
+    public void setLebenspunkte(int lebenspunkte) {
+        this.lebenspunkte = lebenspunkte;
+    }
+
+    public void setPunkte(int punkte) {
+        this.punkte = punkte;
+    }
+
+    public void setDecks(ArrayList<Deck> decks) {
+        this.decks = decks;
+    }
+
+    public void setMomentanesDeck(Deck momentanesDeck) {
+        this.momentanesDeck = momentanesDeck;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public void setSpielfeldseite(Spielfeldseite spielfeldseite) {
+        this.spielfeldseite = spielfeldseite;
+    }
+
+    public void setFriedhof(Friedhof friedhof) {
+        this.friedhof = friedhof;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRang() {
+        return rang;
+    }
+
+    public int getLebenspunkte() {
+        return lebenspunkte;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+    public ArrayList<Deck> getDecks() {
+        return decks;
+    }
+
+    public Deck getMomentanesDeck() {
+        return momentanesDeck;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public Spielfeldseite getSpielfeldseite() {
+        return spielfeldseite;
+    }
+
+    public Friedhof getFriedhof() {
+        return friedhof;
+    }
+    // andere
 
 
 }
