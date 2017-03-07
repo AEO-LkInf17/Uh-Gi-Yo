47ba663048eb0358e16750ae4b7181ee903f9164
julian-20 <julian20.delta@gmail.com>
2017-03-07 11:24:17 +0100

    add Packets in java

---
+++ server/src/de/aeoinf17/project_x17/server/communication/packet/Packet.java
@@ -0,0 +1,8 @@
+package de.aeoinf17.project_x17.server.communication.packet;
+
+/**
+ * Created by jk0521 on 07.03.2017.
+ */
+public abstract class Packet {
+    public abstract void handlePacket();
+}
