6c8d60f87bd72895038a92c499591ea39164326c
thesenwitz <MarvinT@t-online.de>
2017-03-24 08:30:41 +0100

    new project structure

--- core/src/de/aeoinf17/project_x17/Eigenschaft.java
+++
@@ -1,8 +0,0 @@
-package de.aeoinf17.project_x17;
-
-class Propertie {
-    private String bezeichnung;
-
-    public Propertie (String a){bezeichnung = a;}
-
-}
--- server/src/server/Main.java
+++ server/src/server/Main.java
@@ -1,9 +1,5 @@
 package server;
 
-
-/**
- * Created by jk0521 on 24.02.2017.
- */
 public class Main {
     public static void main(String args[]) {
 
