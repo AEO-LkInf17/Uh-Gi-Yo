073200736937ebedb839c0562208ef03f7c18aa2
KosmosSzy <cB0502Asx>
2017-04-05 13:18:52 +0200

    Standart Game

--- server/src/server/logic/Game.java
+++ server/src/server/logic/Game.java
@@ -1,6 +1,5 @@
 package server.logic;
 
-
 import java.util.ArrayList;
 
 /**Die Klasse Game beinhaltet eine Schleife, die den ganzen Spielverlauf organisiert. Das beinhaltet auch die einzelnen Phasen eines Zugs.
@@ -14,13 +13,15 @@ public class Game {
     private static final int MAIN_PHASE_II = 4;
     private static final int END_PHASE = 5;
 
-    private ArrayList<Player> players;
+    private Player challenger;
+    private Player opponent;
 
     private int phase;
     private Player currentPlayersTurn;
     private int round;
 
-    public Game(){phase = 0;round = 0;}
+
+    public Game(Player c,Player o){phase = 0;round = 0;challenger = c;opponent = o;}
 
     public void setPhase(int p){phase = p;}
     public int getPhase(){return phase;}
@@ -29,9 +30,24 @@ public class Game {
     public void setRound(int r){round = r;}
     public int getRound(){return round;}
 
-    public void addPlayer(Player p){players.add(p);}
+    public void nextPhase(){
+        if (phase<5){phase = phase +1;}
+        else{phase = 0;}
+    }
 
     public void createStandartGame(){
+        Hand h1 = new Hand(challenger);
+        Hand h2 = new Hand(opponent);
+        Field f1 = new Field(challenger);
+        Field f2 = new Field(opponent);
+        Graveyard g1 = new Graveyard(challenger);
+        Graveyard g2 = new Graveyard(opponent);
+        challenger.setLifepoints(8000);
+        opponent.setLifepoints(8000);
+    }
+
+    public void startStandartGame(){
+        createStandartGame();
 
     }
 
