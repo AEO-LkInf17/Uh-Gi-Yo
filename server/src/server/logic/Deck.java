84060004f4fdc8898d2ce53e3523267663f4cf7f
samardzic <batica_mali@hotmail.com>
2017-04-07 08:54:28 +0200

    JavaDoc comments

--- server/src/server/logic/Deck.java
+++ server/src/server/logic/Deck.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 /**The class Deck is there to create and save a deck, also to know his details (how many cards, which is the top card, to shuffle)
  *@author Simo ß Minh
- * @version
+ * @version 1.0
  */
 public class Deck {
     private String description;
