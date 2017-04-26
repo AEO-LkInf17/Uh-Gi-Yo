b3a4c639e923b53b40c4fafb0f32a471c49f135a
thesenwitz <MarvinT@t-online.de>
2017-04-26 13:33:05 +0200

    edit GUI

--- client/src/client/core/Game.java
+++ client/src/client/core/Game.java
@@ -2,22 +2,13 @@ package client.core;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import client.core.managers.AssetManager;
 import client.core.managers.ScreenManager;
-import client.core.screens.ScreenEnum;
 
 public class Game implements ApplicationListener {
 
-    public SpriteBatch batch;
-
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        AssetManager.getInstance().initialize("test.json");
-
         ScreenManager.getInstance().initialize(this);
-        ScreenManager.getInstance().setScreen(ScreenEnum.SPLASH);
     }
 
     @Override
@@ -41,8 +32,6 @@ public class Game implements ApplicationListener {
     @Override
     public void dispose() {
         ScreenManager.getInstance().dispose();
-        AssetManager.getInstance().dispose();
-        batch.dispose();
     }
 }
 
--- client/src/client/core/screens/MainMenuScreen.java
+++ client/src/client/core/screens/MainMenuScreen.java
@@ -1,35 +1,11 @@
 package client.core.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import client.core.Game;
-import client.core.managers.AssetManager;
 
-
-public class MainMenuScreen extends Screen {
-
-    private Texture texture;
-    private Music music;
-
+public class MainMenuScreen extends AbstractScreen {
 
     public MainMenuScreen(Game game) {
         super(game);
-
-        AssetManager.getInstance().load("group 1");
-        AssetManager.getInstance().finishLoading();
-        System.out.println("SUCCESS!");
-        texture = AssetManager.getInstance().get("badlogic.jpg", Texture.class);
-        music = AssetManager.getInstance().get("intro.wav", Music.class); // the instance in the assets will be brought here with a path
-        music.play(); //plays music
-        music.setVolume((float)1.0);
-        music.getVolume();
-        music.setLooping(true);
-        music.isLooping();
-
-
-
     }
 
     @Override
@@ -37,20 +13,7 @@ public class MainMenuScreen extends Screen {
     }
 
     @Override
-    public void update(float delta) {
-
-    }
-
-    @Override
     public void render(float delta) {
-        super.render(delta);
-
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
     }
 
     @Override
@@ -75,6 +38,5 @@ public class MainMenuScreen extends Screen {
 
     @Override
     public void dispose() {
-        texture.dispose();
     }
 }
