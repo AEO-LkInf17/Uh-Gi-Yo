8ef471fbf4af7cb48d9215eedb798bfa63f02da1
julian-20 <julian20.delta@gmail.com>
2017-04-26 13:25:05 +0200

    add sql instance in Main

--- server/src/server/Main.java
+++ server/src/server/Main.java
@@ -6,6 +6,7 @@ import server.communication.Server;
 import server.communication.packet.IncomingPacket;
 import server.communication.packet.packets.incoming.LoginPacket;
 import server.communication.packet.packets.incoming.RegisterPacket;
+import server.sql.SQL;
 import server.user.User;
 
 import java.io.BufferedReader;
@@ -13,15 +14,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class Main {
     public static final String VERSION = "0.0-dev";
     public static final Gson GSON = new Gson();
 
     private static Server server = new Server();
+    private static SQL sql = new SQL();
 
     public static Server getServer() {
         return server;
+    }
+    public static SQL getSql() {
+        return sql;
     }
 
     public static void main(String args[]) {
