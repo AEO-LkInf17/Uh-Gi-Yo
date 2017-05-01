afb7c3e6ca484fb67f2e3002adba5c3c558b3581
Zuck3rwatt3 <MarvinT@t-online.de>
2017-05-01 18:11:58 +0200

    edit structure, gui

--- client/src/client/launcher/DesktopLauncher.java
+++ client/src/client/launcher/DesktopLauncher.java
@@ -2,13 +2,14 @@ package client.launcher;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import client.core.Game;
+import client.core.Application;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.height = 600;
         config.width  = 800;
-        new LwjglApplication(new Game(), config);
+        config.title  = "";
+        new LwjglApplication(new Application(), config);
     }
 }
