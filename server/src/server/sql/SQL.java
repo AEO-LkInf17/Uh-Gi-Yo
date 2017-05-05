423b830d5dcc103f0763f53dbab9116abce9ec83
mk0458 <mauriceklamet@web.de>
2017-05-05 09:31:08 +0200

    ...

--- server/src/server/sql/SQL.java
+++ server/src/server/sql/SQL.java
@@ -25,22 +25,23 @@ public class SQL {
 		try {
 			System.out.println("SQL init started");
 			Class.forName("com.mysql.jdbc.Driver");
-			
+
 			String host = "dbserver-w10-die.einstein";
-			String databaseName="UhGiYo_INFLK17";
+			String databaseName = "UhGiYo_INFLK17";
 			String userName = "root";
 			String password = "w10";
 
-			con = DriverManager.getConnection("jdbc:mysql://"+host+":3306/"+databaseName,userName,password);
+			con = DriverManager.getConnection("jdbc:mysql://" + host + ":3306/" + databaseName, userName, password);
 			st = con.createStatement();
 			System.out.println("SQL init succeed");
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println("Error: " + ex);
 		}
 	}
-	
+
 	/**
 	 * checks whether the user exists (login)
+	 *
 	 * @param username the username of the user
 	 * @return whether the user exists or not
 	 */
@@ -48,43 +49,42 @@ public class SQL {
 		try {
 			String query = "SELECT username FROM users";
 			rs = st.executeQuery(query);
-			while(rs.next()) {
-		        String un = rs.getString("username");
-			    if(username.equals(un)) {
-			        return true;
-			    }
-		    } 
-	    } catch(SQLException e) {
-	    	e.printStackTrace();
-	      }
+			while (rs.next()) {
+				String un = rs.getString("username");
+				if (username.equals(un)) {
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
-	
+
 	/**
 	 * checks whether the user is banned
+	 *
 	 * @param username the username of the user
-	 * @param bl the time of how long the user is banned
 	 * @return whether the user is banned or not
 	 */
-	public boolean userIsBanned(String username, int bl) {
+	public boolean userIsBanned(String username) {
 		try {
 			String query = "SELECT username FROM users WHERE username = '" + username + "'";
 			rs = st.executeQuery(query);
-			while(rs.next()) {
-				int banLength = Integer.parseInt(rs.getString("banLength"));
-			    if(banLength > 0) { // banned = true
-					bl = banLength;
+			while (rs.next()) {
+				int banned = Integer.parseInt(rs.getString("banned"));
+				if (banned == 1 || banned == -1)
 					return true;
-			    }
 			}
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		  }
+		}
 		return false;
 	}
-	
+
 	/**
 	 * saves the new username in the database (registration)
+	 *
 	 * @param username the username to be saved
 	 */
 	public void saveNewUsername(String username) {
@@ -98,8 +98,9 @@ public class SQL {
 
 	/**
 	 * changes the login status of the user
+	 *
 	 * @param loginStatus the value of whether the user is going to be logged in or out
-	 * @param username the username of the user
+	 * @param username    the username of the user
 	 */
 	public void changeLoginStatusTo(boolean loginStatus, String username) {
 		try {
@@ -112,19 +113,20 @@ public class SQL {
 
 	/**
 	 * changes the time of how long the user is banned
-	 * @param banLength the time of how long the user is banned
+	 *
 	 * @param username the username of the user that is going to be banned
 	 */
-	public void changeBanLengthTo(int banLength, String username) {
+	public void changeBannedUntilTo(int bannedUntil, String username) {
 		try {
-			st.executeUpdate("UPDATE users SET banLength = " + banLength + "WHERE username = " + username + ";");
+			st.executeUpdate("UPDATE users SET bannedUntil = " + bannedUntil + "WHERE username = " + username + ";");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * checks whether the username is available (registration)
+	 *
 	 * @param username the username that has been typed in during the registration
 	 * @return whether the username is available or not
 	 */
@@ -132,15 +134,22 @@ public class SQL {
 		try {
 			String query = "SELECT username FROM users";
 			rs = st.executeQuery(query);
-			while(rs.next()) {
-			    String un = rs.getString("username");
-			    if(username.equals(un)) {
-			    	return false;
-			    }
+			while (rs.next()) {
+				String un = rs.getString("username");
+				if (username.equals(un)) {
+					return false;
+				}
 			}
-		} catch(SQLException e) {
-			    e.printStackTrace();
-			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return true;
+	}
+
+
+	public void bannedTimeIsOver(String username, int bannedUntil) {
+		try {
+			String query = "SELECT bannedUntil  FROM users WHERE bannedUntil = '" + bannedUntil + ";";
+		}
 	}
 }
