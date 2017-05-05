02e6e767b9a16c7b9db13a6efc53ccc0991550dc
KosmosSzy <cB0502Asx>
2017-05-05 08:44:37 +0200

    Activity

--- server/src/server/logic/Activity/Activities/SummonActivity.java
+++ server/src/server/logic/Activity/Activities/SummonActivity.java
@@ -1,7 +1,18 @@
 package server.logic.Activity.Activities;
 
 import server.logic.Activity.Activity;
+import server.logic.cards.MonsterCard;
 
 public class SummonActivity extends Activity {
 
+    private MonsterCard target;
+
+    public SummonActivity (MonsterCard m){
+        target = m;
+    }
+
+    @Override
+    public void handleActivity() {
+
+    }
 }
