c91fee3663a97617775b4d35c5f5a3b6f166db7f
thesenwitz <MarvinT@t-online.de>
2017-05-10 13:31:45 +0200

    edit

---
+++ client/src/client/core/gui/elements/ProgressCircle.java
@@ -0,0 +1,15 @@
+package client.core.gui.elements;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ProgressCircle extends Actor {
+
+    private float progess;
+    private int progressInPersent;
+
+    public ProgressCircle() {
+
+    }
+
+
+}
---
+++ client/src/client/core/gui/utils/Constants.java
@@ -0,0 +1,13 @@
+package client.core.gui.utils;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Constants {
+    // text
+    public static final String LOADING = "LOADING";
+
+    // colors
+    public static final Color WHITE = new Color();
+    public static final Color RED = new Color();
+    public static final Color GRAY = new Color();
+}
